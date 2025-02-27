96ac81b35478c0870b7dda76d2612c4f90a10b8c
Ian <ianrp.lin@gmail.com>
2025-02-27 15:59:55 -0500

    Add bare minimum for throwing motor contorl

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-from inputs import initialize_imu, smooth_readings
 import time
 from collections import deque
+import RPi.GPIO as GPIO
 
+from imu import initialize_imu, smooth_readings
+from throwing import throwing_speed
+
+# IMU
 refresh_rate = 0.5
 moving_average_window = 10
 yaw_buffer = deque(maxlen=moving_average_window)
 pitch_buffer = deque(maxlen=moving_average_window)
 roll_buffer = deque(maxlen=moving_average_window)
 
+# Throwing
+PWM_PIN = 13  # Physical pin 33
+PWM_FREQ = 500
+PWM_INIT_DC = 0
+
+
 if __name__ == '__main__':
 
     bno = initialize_imu()
 
+    # Setup
+    GPIO.setmode(GPIO.BCM)  # Use Broadcom GPIO pin numbering
+    GPIO.setup(PWM_PIN, GPIO.OUT)
+
+    # Initialize PWM on the pin with a frequency of 100 Hz
+    pwm = GPIO.PWM(PWM_PIN, 500)
+    pwm.start(PWM_INIT_DC)  # Start PWM with 0% duty cycle
+
     while True:
         yaw, pitch, roll = smooth_readings(bno, yaw_buffer, pitch_buffer, roll_buffer)
 
+        distance = 10   # To be replaced with CV distance estimate
+        
+        pwm.ChangeDutyCycle(throwing_speed(distance))
+        
         time.sleep(refresh_rate)
---
+++ throwing.py
@@ -0,0 +1,14 @@
+import time
+
+
+def throwing_speed(distance):
+    """
+    Adjust motor speed based on distance obtained from CV
+
+    :param distance: distance
+    :return: PWM duty cycle
+    """
+
+    speed = distance
+
+    return speed
