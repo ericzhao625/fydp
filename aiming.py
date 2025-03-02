41c868ba169287747627f477cb594ae6876e13fb
Ian <ianrp.lin@gmail.com>
2025-03-02 13:31:50 -0500

    Pass constants into class parameters

--- aiming.py
+++ aiming.py
@@ -1,8 +1,15 @@
 from h_bridge import HBridge
-
+import constants
 
 class Aim(HBridge):
-    def __init__(self, in1, in2, enable, pwm_freq, pwm_dc):
+    def __init__(
+        self,
+        in1=constants.IN7,
+        in2=constants.IN8,
+        enable=constants.ENABLE_D,
+        pwm_freq=constants.AIMING_MOTOR_FREQ,
+        pwm_dc=constants.AIMING_MOTOR_DC
+        ):
         super().__init__(in1, in2, enable, pwm_freq, pwm_dc)
     
     def track_player(self, pose_estimation):
