7b1ea0b0f336b50194f99ce30c3a610b9df6cb18
Ian <ianrp.lin@gmail.com>
2025-03-02 01:46:26 -0500

    Add CV code to main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import mediapipe as mp
 
 from imu import IMU
 from throwing import throwing_speed
-from cv import pose_detection
+from cv import CV
 
 import constants
 
@@ -17,41 +17,27 @@ if __name__ == '__main__':
     # Initialize IMU
     imu = IMU()
 
-    pinLED = 17
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(pinLED, GPIO.OUT)
+    # Initialize CV
+    cv = CV()
 
-    # Initialize MediaPipe Pose
-    mp_pose = mp.solutions.pose
-    pose = mp_pose.Pose()
+    while True:
+        frame = cv.read_frame()
+        frame_rgb, pose_results = cv.process_frame(frame)
+        joints = cv.extract_joints(pose_results)
 
-    # Initialize camera
-    cap = cv2.VideoCapture(0)  # Use 0 for the default camera
+        distance = cv.smooth_distance(frame, joints)
+        # print(f'Distance: {distance}m')
 
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-    cap.set(cv2.CAP_PROP_FPS, 5)  # Attempt to set FPS to 30
-    fps = cap.get(cv2.CAP_PROP_FPS)  # Check if the setting was successful
-    print(f"Camera FPS: {fps}")
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-
-        if not ret:
-            break
-
-        distance = pose_detection(mp_pose, pose, frame)
-        print(distance)
+        pose_estimation = cv.pose_estimation(frame, joints)
+        # print(f'Pose Estimation: {pose_estimation}')
 
         yaw, pitch, roll = imu.smooth_readings()
 
         # Show the video feed with the landmarks
-        cv2.imshow("Pose Detection", frame)
+        cv2.imshow("Frisbeast Vision", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    cap.release()
+    cv.cap_release()
     cv2.destroyAllWindows()
