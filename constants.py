1be4dca69bc57e5bcd32999f12ba621e30d5806f
Ian <ianrp.lin@gmail.com>
2025-03-01 20:52:51 -0500

    Refactor main and add constants file

---
+++ constants.py
@@ -0,0 +1,9 @@
+# Throwing
+PWM_PIN = 12  # Physical pin 33
+PWM_FREQ = 500
+PWM_INIT_DC = 0
+
+# Computer vision
+PIN_LED = 17
+CENTER = 0.5
+POSE_TOLERANCE = 0.2
--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import time
 from collections import deque
 import RPi.GPIO as GPIO
 
+import cv2
+import mediapipe as mp
+
 from imu import initialize_imu, smooth_readings
 from throwing import throwing_speed
+from cv import pose_detection
+
+import constants
 
 # IMU
 refresh_rate = 0.5
@@ -12,29 +18,52 @@ yaw_buffer = deque(maxlen=moving_average_window)
 pitch_buffer = deque(maxlen=moving_average_window)
 roll_buffer = deque(maxlen=moving_average_window)
 
-# Throwing
-PWM_PIN = 13  # Physical pin 33
-PWM_FREQ = 500
-PWM_INIT_DC = 0
-
-
 if __name__ == '__main__':
 
-    bno = initialize_imu()
+    pinLED = 17
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(pinLED, GPIO.OUT)
 
-    # Setup
-    GPIO.setmode(GPIO.BCM)  # Use Broadcom GPIO pin numbering
-    GPIO.setup(PWM_PIN, GPIO.OUT)
+    # # MediaPipe
+    # center = 0.5
+    # pose_tolerance = 0.2
 
-    # Initialize PWM on the pin with a frequency of 100 Hz
-    pwm = GPIO.PWM(PWM_PIN, 500)
-    pwm.start(PWM_INIT_DC)  # Start PWM with 0% duty cycle
+    # Initialize MediaPipe Pose
+    mp_pose = mp.solutions.pose
+    pose = mp_pose.Pose()
 
-    while True:
-        yaw, pitch, roll = smooth_readings(bno, yaw_buffer, pitch_buffer, roll_buffer)
+    # Initialize camera
+    cap = cv2.VideoCapture(0)  # Use 0 for the default camera
 
-        distance = 10   # To be replaced with CV distance estimate
-        
-        pwm.ChangeDutyCycle(throwing_speed(distance))
-        
-        time.sleep(refresh_rate)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+    cap.set(cv2.CAP_PROP_FPS, 5)  # Attempt to set FPS to 30
+    fps = cap.get(cv2.CAP_PROP_FPS)  # Check if the setting was successful
+    print(f"Camera FPS: {fps}")
+
+    prev_time = 0
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        # frame = cv2.resize(frame, (640, 480))
+        # Flip the frame horizontally
+        # flipped_frame = cv2.flip(frame, 1)
+        if not ret:
+            break
+
+        pose_detection(mp_pose, pose, frame)
+
+        # Display memory usage
+        # cv2.putText(frame, monitor_memory(), (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        # Show the video feed with the landmarks
+        cv2.imshow("Pose Detection", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
