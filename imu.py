734676d1b9bd8e21a14a391e93c21669c8b08531
Ian <ianrp.lin@gmail.com>
2025-03-01 21:31:10 -0500

    Implement IMU code with OOP

--- imu.py
+++ imu.py
@@ -1,117 +1,125 @@
-import time
 import board
 import busio
 from adafruit_bno08x import BNO_REPORT_ROTATION_VECTOR
 from adafruit_bno08x.i2c import BNO08X_I2C
 from scipy.spatial.transform import Rotation as R
+from collections import deque
 
 
-def initialize_imu():
-    """
-    Initialize IMU to get readings.
+class IMU:
+    def __init__(self, buffer_size=10):
+        """
+        Initialize the IMU sensor and buffers for smoothing.
+        """
+        self.buffer_size = buffer_size
+        self.yaw_buffer = deque(maxlen=buffer_size)
+        self.pitch_buffer = deque(maxlen=buffer_size)
+        self.roll_buffer = deque(maxlen=buffer_size)
+    
+        self.bno = self.initialize_imu()
 
-    :return: IMU initialization
-    """
-    try:
-        # Initialize I2C
-        i2c = busio.I2C(board.SCL, board.SDA)
-        bno = BNO08X_I2C(i2c)
+    def initialize_imu(self):
+        """
+        Initialize IMU to get readings.
 
-        # Enable Quaternion readings for sensor
-        bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+        :return: IMU initialization
+        """
+        try:
+            # Initialize I2C
+            i2c = busio.I2C(board.SCL, board.SDA)
+            bno = BNO08X_I2C(i2c)
 
-        return bno
+            # Enable Quaternion readings for sensor
+            bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
 
-    except Exception as e:
-        print(f'IMU initialization failed: {e}')
+            return bno
+
+        except Exception as e:
+            print(f'IMU initialization failed: {e}')
+            return None
+
+
+    def read_quaternion(self):
+        """
+        Get Quaternion readings from IMU.
+
+        :param bno: Initialized sensor
+        :return: Quaternion readings
+        """
+
+        try:
+            quat_i, quat_j, quat_k, quat_real = self.bno.quaternion
+
+            return quat_i, quat_j, quat_k, quat_real
+        
+        except KeyError as e:
+            print(f'KeyError: IMU returned unexpected data format: {e}')
+        except OSError as e:
+            print(f'OSError: Possible I2C disconnection: {e}')
+        except Exception as e:
+            print(f'Unexpected error reading gyro: {e}')
+
         return None
 
 
-def read_quaternion(bno):
-    """
-    Get Quaternion readings from IMU.
+    def quaternion_to_euler(self, w, x, y, z):
+        """
+        Convert Quaternion readings to Euler angles.
 
-    :param bno: Initialized sensor
-    :return: Quaternion readings
-    """
+        :param w: scalar (real) part
+        :param x: vector (imaginary) part
+        :param y: vector (imaginary) part
+        :param z: vector (imaginary) part
+        :return: yaw, pitch, roll
+        """
+        
+        quaternion_readings = R.from_quat([x, y, z, w])
+        euler_angles = quaternion_readings.as_euler('zyx', degrees=True)
 
-    try:
-        quat_i, quat_j, quat_k, quat_real = bno.quaternion
-
-        return quat_i, quat_j, quat_k, quat_real
-    
-    except KeyError as e:
-        print(f'KeyError: IMU returned unexpected data format: {e}')
-    except OSError as e:
-        print(f'OSError: Possible I2C disconnection: {e}')
-    except Exception as e:
-        print(f'Unexpected error reading gyro: {e}')
-
-    return None
+        return euler_angles
 
 
-def quaternion_to_euler(w, x, y, z):
-    """
-    Convert Quaternion readings to Euler angles.
+    def imu_readings(self):
+        """
+        Get IMU readings.
 
-    :param w: scalar (real) part
-    :param x: vector (imaginary) part
-    :param y: vector (imaginary) part
-    :param z: vector (imaginary) part
-    :return: yaw, pitch, roll
-    """
-    
-    quaternion_readings = R.from_quat([x, y, z, w])
-    euler_angles = quaternion_readings.as_euler('zyx', degrees=True)
+        :param bno: Initialized sensor
+        :return: yaw, pitch, roll
+        """
 
-    return euler_angles
+        # Get Quaternion readings from IMU
+        quat_i, quat_j, quat_k, quat_real = self.read_quaternion()
+        # print(f'I: {quat_i:0.6f} J: {quat_j:0.6f} K: {quat_k:0.6f} Real: {quat_real:0.6f}')
 
+        # Convert Quaternion readings to Euler angles
+        yaw, pitch, roll = self.quaternion_to_euler(quat_i, quat_j, quat_k, quat_real)
+        # print(f'Yaw: {yaw:0.6f} Pitch: {pitch:0.6f} Roll: {roll:0.6f}')
 
-def imu_readings(bno):
-    """
-    Get IMU readings.
-
-    :param bno: Initialized sensor
-    :return: yaw, pitch, roll
-    """
-
-    # Get Quaternion readings from IMU
-    quat_i, quat_j, quat_k, quat_real = read_quaternion(bno)
-    # print(f'I: {quat_i:0.6f} J: {quat_j:0.6f} K: {quat_k:0.6f} Real: {quat_real:0.6f}')
-
-    # Convert Quaternion readings to Euler angles
-    yaw, pitch, roll = quaternion_to_euler(quat_i, quat_j, quat_k, quat_real)
-    # print(f'Yaw: {yaw:0.6f} Pitch: {pitch:0.6f} Roll: {roll:0.6f}')
-
-    return yaw, pitch, roll
+        return yaw, pitch, roll
 
 
-def smooth_readings(bno, yaw_buffer, pitch_buffer, roll_buffer):
-    """
-    Remove outliers by using a low pass filter.
+    def smooth_readings(self):
+        """
+        Remove outliers by using a low pass filter.
 
-    :param bno: Initialized sensor
-    :param yaw_buffer: buffer to store yaw previous values
-    :param pitch_buffer: buffer to store pitch previous values
-    :param roll_buffer: buffer to store roll previous values
-    :return: averaged yaw, pitch, and roll values
-    """
-    try:
-        yaw, pitch, roll = imu_readings(bno)
+        :return: averaged yaw, pitch, and roll values
+        """
+        try:
+            yaw, pitch, roll = self.imu_readings()
 
-        yaw_buffer.append(yaw)
-        pitch_buffer.append(pitch)
-        roll_buffer.append(roll)
+            self.yaw_buffer.append(yaw)
+            self.pitch_buffer.append(pitch)
+            self.roll_buffer.append(roll)
 
-        averaged_yaw = sum(yaw_buffer) / len(yaw_buffer)
-        averaged_pitch = sum(pitch_buffer) / len(pitch_buffer)
-        averaged_roll = sum(roll_buffer) / len(roll_buffer)
-        print(f'Yaw: {averaged_yaw:.6f} Pitch: {averaged_pitch:.6f} Roll: {averaged_roll:.6f}')
+            averaged_yaw = sum(self.yaw_buffer) / len(self.yaw_buffer)
+            averaged_pitch = sum(self.pitch_buffer) / len(self.pitch_buffer)
+            averaged_roll = sum(self.roll_buffer) / len(self.roll_buffer)
+            print(f'Yaw: {averaged_yaw:.6f} Pitch: {averaged_pitch:.6f} Roll: {averaged_roll:.6f}')
 
-        return averaged_yaw, averaged_pitch, averaged_roll
+            return averaged_yaw, averaged_pitch, averaged_roll
 
-    except ValueError as e:
-        print(f'Exception: IMU did not get data readings: {e}')
-    except TypeError as e:
-        print(f'Exception: {e}')
-    return None, None, None
+        except ValueError as e:
+            print(f'Exception: IMU did not get data readings: {e}')
+        except TypeError as e:
+            print(f'Exception: {e}')
+        return None, None, None
