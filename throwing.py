fc13c46c167e55b37ac3687265c0c36cc6935bd0
Ian <ianrp.lin@gmail.com>
2025-03-07 22:05:59 -0500

    Make print statement more clear

--- throwing.py
+++ throwing.py
@@ -96,7 +96,7 @@ class Throw:
         """
         pwm_value = speed / 100 * self.max_pwm
         self.pwm.ChangeDutyCycle(pwm_value)
-        print(pwm_value)
+        print(f'Updated PWM from app: {pwm_value}')
 
     def push_frisbee(self, distance, pose_estimation):
         """
