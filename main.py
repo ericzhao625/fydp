17b652d4184a2859a1e9110f1ee75d1712764917
Ian <ianrp.lin@gmail.com>
2025-03-08 11:01:06 -0500

    Remove unnecessary imports

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import RPi.GPIO as GPIO
-import cv2
-
-from aiming import Aim
 from bluetooth_communication import Bluetooth
-from cv import CV
-from imu import IMU
-from signal import pause
-from throwing import Throw
-from utils import display_metrics
 
 
 if __name__ == '__main__':
