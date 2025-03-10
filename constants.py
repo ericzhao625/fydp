68922cd5621177c50da93811390effe97434c939
Eric Zhao <e23zhao@uwaterloo.ca>
2025-03-09 20:56:57 -0400

    Updates to pin constants

--- constants.py
+++ constants.py
@@ -1,20 +1,17 @@
 # IMU
 IMU_BUFFER_SIZE = 10
 
-# Linear Actuators
-LINEAR_ACTUATOR_FREQ = 100
-LINEAR_ACTUATOR_DC = 50
-# Linear Actuator 1
-IN1 = 14
-IN2 = 15
+# Linear Actuator Left Front out3 out4
+IN3 = 25
+IN4 = 24
+ENABLE_B = 23
+
+# Linear Actuator Left Back out1 out2
+IN1 = 15
+IN2 = 14
 ENABLE_A = 18
 
-# Linear Actuator 2
-IN3 = 23
-IN4 = 24
-ENABLE_B = 25
-
-# Linear Actuator 3
+# Linear Actuator Right
 IN5 = 10
 IN6 = 9
 ENABLE_C = 11
@@ -22,8 +19,8 @@ ENABLE_C = 11
 # Aiming Motor
 AIMING_MOTOR_FREQ = 100
 AIMING_MOTOR_DC = 25
-IN7 = 17
-IN8 = 27
+IN7 = 27
+IN8 = 17
 ENABLE_D = 22
 AIMING_DEADBAND = 1 # Degrees
 KP = 1
